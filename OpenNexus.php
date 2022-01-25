6616e9c11ad6ea8dd149a4965e5c0b0342e39e2b
bmgjet <50484759+bmgjet@users.noreply.github.com>
2022-01-25 21:53:46 +1300

    php server

--- OpenNexus.php
+++ OpenNexus.php
@@ -1,13 +1,21 @@
 <?php
 //Setup Key that needs to be provided as user-agent for php script to react
 $Key = 'bmgjet123';
-
+error_reporting(0);
 //Checks DataFolders Exsists
-if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/ON/')) {
-    mkdir($_SERVER['DOCUMENT_ROOT'] . '/ON/', 0777, true);
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/')) {
+    mkdir($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/', 0777, true);
 }
-if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/ON-SENT/')) {
-    mkdir($_SERVER['DOCUMENT_ROOT'] . '/ON-SENT/', 0777, true);
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Packets/')) {
+    mkdir($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Packets/', 0777, true);
+}
+
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Old/')) {
+    mkdir($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Old/', 0777, true);
+}
+
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/')) {
+    mkdir($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/', 0777, true);
 }
 
 //Make sure its a POST Event and not a GET
@@ -16,9 +24,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 //get POST data
 $entityBody = file_get_contents('php://input');
 
-//Make sure its base64 for some protection against code injection (Only Supports Compression Mode)
-if (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $entityBody)){echo 'Bad data'; return;}
-
 //Check useragent provided matches the key
 $file = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
 if($file != $Key) { echo 'invalid key'; return;}
@@ -26,57 +31,90 @@ if($file != $Key) { echo 'invalid key'; return;}
 //Setup Variables
 $TARGET = '';
 $FROM = '';
+$Command = '';
 $headers = apache_request_headers();
 foreach ($headers as $header => $value) 
 {
 	if($header == 'TARGET') {$TARGET = $value;}
 	if($header == 'FROM') {$FROM = $value;}
+	if($header == 'CMD') {$Command = $value;}
 }
 //Check data has a Target and From IP-Port
 if($TARGET == '' || $FROM == '') { echo 'Bad Data'; return;}
 
-//Ping Request to Sync Servers
-if($entityBody == 'PING') 
-{ 
-//Create Sync Data
-if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/ON-SYNC/')) {
-    mkdir($_SERVER['DOCUMENT_ROOT'] . '/ON-SYNC/', 0777, true);
-}
-//Create sync file from ip/port
-if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/ON-SYNC/'.$FROM)) {
-file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/ON-SYNC/'.$FROM, $entityBody);
-}
-//if less then 2 sync files dont sync yet
-$amount = count(glob($_SERVER['DOCUMENT_ROOT'] . '/ON-SYNC/' . "*"));
-if($amount < 2) { echo 'WAIT'; return;}
-//send 10 sec delay before departing
-echo '10';
-return;
-}
-
-//Remove Sync Data
-$oldfiles = glob($_SERVER['DOCUMENT_ROOT'] . '/ON-SYNC/' . "*");
-foreach($oldfiles as $kill)
-{
-  if(is_file($kill)) 
-  {
-    unlink($kill);
-  }
-}
-
-
 //Create time stamp
 $date = date_create();
 
-//Create file
-$filepath = $_SERVER['DOCUMENT_ROOT'] . '/ON/' . $TARGET . '-' . date_timestamp_get($date);
-file_put_contents($filepath, $entityBody);
-
-//Sleep for 100ms to allow files to read/write
+//Ping Request to Sync Servers
+if($Command == 'PING') 
+{ 
+//Create sync file from ip/port
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/'.$FROM, $entityBody);
+//delay 100ms for file io
 usleep(100000);
+//if less then 2 sync files dont sync yet
+$amount = glob($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/' . "*", GLOB_NOSORT);
+if(count($amount) < 2) { echo 'WAIT'; return;}
+//Create a time stamp
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/TimeStamp.txt')) 
+{
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/TimeStamp.txt', date_timestamp_get($date));
+usleep(100000);
+}
+//echos timestamp from file + servers time stamp to work out a time to sync at.
+include($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/TimeStamp.txt');
+echo ' ' . date_timestamp_get($date);
+return;
+}
 
-//Fild files that this server wants
-$matches = glob(realpath('./ON') . '/'. $FROM .'*', GLOB_NOSORT);
+//Removes time stamp file
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/TimeStamp.txt')) 
+{
+unlink($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/TimeStamp.txt');
+return;
+}
+
+//Remove sync file
+if($Command == 'LEAVE') 
+{ 
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/'.$FROM)) 
+{
+unlink($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/'.$FROM);
+return;
+}
+}
+
+//Fill sync file with current ferry status
+if($Command == 'SYNC') 
+{ 
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/'.$FROM, $entityBody);
+return;
+}
+
+//Checks from and target sync files have same status
+if($Command == 'READY') 
+{ 
+if(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/'.$FROM) === file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Sync/'.$TARGET))
+{
+	echo 'true';
+	return;
+}
+echo 'false';
+return;
+}
+
+//Store a entity packet
+if($Command == 'WRITE') 
+{ 
+if($entityBody != "")
+{
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Packets/' . $TARGET . '-' . date_timestamp_get($date), $entityBody);
+return;
+}
+}
+
+//Find files that this server wants
+$matches = glob($_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Packets/' . $FROM .'*', GLOB_NOSORT);
 //Sort so newest listed first
 array_multisort(array_map('filemtime', $matches), SORT_NUMERIC, SORT_DESC, $matches);
 foreach ($matches as $filen) 
@@ -85,12 +123,10 @@ foreach ($matches as $filen)
 	include $filen;
 	//Move file to old filder
 	$source_file = $filen;
-	$destination_path = $_SERVER['DOCUMENT_ROOT'] . '/ON-SENT/';
+	$destination_path = $_SERVER['DOCUMENT_ROOT'] . '/OpenNexus/Old/';
 	rename($source_file, $destination_path . pathinfo($source_file, PATHINFO_BASENAME));
 	//End function
 	return;
 }
-//End of POST
-return;
 }
 ?>
